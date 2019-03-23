89b8ac603873ec6501fad41b672f62c72351d261
Ritesh Singh <1124ritesh@gmail.com>
2019-03-23 15:17:55 +0530

    auth completed
    Signed-off-by: Ritesh Singh <1124ritesh@gmail.com>

--- backend/app/http/controllers/auth/registerController.js
+++ backend/app/http/controllers/auth/registerController.js
@@ -21,33 +21,3 @@ exports.register = (req, res) => {
     });
   });
 };
-
-exports.login = (req, res) => {
-  // Find user by email
-  user.findOne({ email: req.body.email }).then(user => {
-    // Check for user
-    if (!user) {
-      // errors.email = "User not found";
-      return res.status(404).json("user not found");
-    }
-
-    // Check Password
-    bcrypt.compare(req.body.password, user.password).then(isMatch => {
-      if (isMatch) {
-        // User Matched
-        const payload = { id: user.id, name: user.name, email: user.email }; // Create JWT Payload
-
-        // Sign Token
-        jwt.sign(payload, key, { expiresIn: 3600 }, (err, token) => {
-          return res.json({
-            success: true,
-            token: "Bearer " + token
-          });
-        });
-      } else {
-        // errors.password = "Password incorrect";
-        return res.status(400).json("Password Incorrect");
-      }
-    });
-  });
-};

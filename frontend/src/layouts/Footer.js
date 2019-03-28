20a98d0795609f0f1cc0265070b1f88d4c989bf8
Ritesh Singh <1124ritesh@gmail.com>
2019-03-28 16:21:35 +0530

    frontend
    Signed-off-by: Ritesh Singh <1124ritesh@gmail.com>

--- frontend/src/layouts/Footer.js
+++ frontend/src/layouts/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default () => {
   return (
     <footer className="bg-white text-dark mt-5 p-2 text-center w-100 footer-mern">
-      Copyright &copy; {new Date().getFullYear()} MernStarter
+      Your Copyright Section goes here
     </footer>
   );
 };
